42af348cf35847e5856696b10135ed4a74542e56
LeBoufty <boufto@gmail.com>
2025-07-27 11:11:13 +0200

    Title n stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div>
+  <div class="col-span-3">
+    <h1 class="text-center text-mafreidyne font-black text-8xl m-4">
+      mafreidyne.zip
+    </h1>
+    <h3 class="text-center text-zinc-400 italic font-bold text-2xl">
+      Nuclear-powered freakhouse of fun
+    </h3>
+  </div>
+</div>
