3fab4a93e0d038c1a371406a99aac0c18c42bee3
LeBoufty <boufto@gmail.com>
2025-08-06 16:14:42 +0200

    Added cards

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,13 @@
-<div>
-  <div class="col-span-3">
-    <h1 class="text-center text-mafreidyne font-black text-8xl m-4">
-      mafreidyne.zip
-    </h1>
-    <h3 class="text-center text-zinc-400 italic font-bold text-2xl">
-      Nuclear-powered freakhouse of fun
-    </h3>
-  </div>
+<script lang="ts">
+  import Card from "../props/Card.svelte";
+</script>
+
+<div class="col-span-3 m-4">
+  <h1 class="text-center text-mafreidyne font-black text-8xl m-4">
+    mafreidyne.zip
+  </h1>
+  <h3 class="text-center text-zinc-400 italic font-bold text-2xl">
+    Nuclear-powered freakhouse of fun
+  </h3>
 </div>
+<Card title="Gedagedigedagedaho">wow yeehaw</Card>
