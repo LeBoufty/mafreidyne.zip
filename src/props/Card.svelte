f5db6789e8df2154071281c5f58df2a9c740a765
LeBoufty <boufto@gmail.com>
2025-08-06 16:15:52 +0200

    i forgor

---
+++ src/props/Card.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  let { title } = $props();
+</script>
+
+<div
+  class="mx-auto max-w-sm gap-x-4 rounded-xl bg-zinc-800 p-4 shadow-xl outline-cyan-500 outline-4"
+>
+  <h2 class="text-center text-zinc-100 font-bold text-2xl">{title}</h2>
+  <hr class="m-4 text-zinc-600" />
+  <p class="text-justify text-zinc-200">
+    <slot />
+  </p>
+</div>
